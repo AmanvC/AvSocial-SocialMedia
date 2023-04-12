cc4249d46750d15022f6b27a4c17fdb0b11900c8
Aman <amanvarshney.varshney@gmail.com>
2023-04-13 01:21:22 +0530

    Completed page not found

--- client/src/pages/404/PageNotFound.jsx
+++ client/src/pages/404/PageNotFound.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import "./pageNotFound.scss";
 
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="page-not-found">
+      <div>
+        <h1>Are you lost?</h1>
+        <p>The page you're looking for does not exist yet!</p>
+      </div>
+      <div>
+        <h3>Don't worry, we'll steer you to safety.</h3>
+        <Link to="/">
+          <button>
+            Go back home <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default PageNotFound;
