f595848f806ded563bedd23558c726bcff120042
Aman <amanvarshney.varshney@gmail.com>
2023-04-20 01:16:57 +0530

    Added rightbar component

--- client/src/components/leftBar/LeftBar.jsx
+++ client/src/components/leftBar/LeftBar.jsx
@@ -1,7 +1,27 @@
 import "./leftBar.scss";
 
+import { Link } from "react-router-dom";
+import { BiLinkExternal } from "react-icons/bi";
+import { RiContactsBook2Fill } from "react-icons/ri";
+
 const LeftBar = () => {
-  return <div className="leftbar">LeftBar</div>;
+  return (
+    <div className="leftbar">
+      <h3>More Apps</h3>
+      <ul>
+        <li>
+          <Link target="_blank" to={"https://imovies-react.netlify.app/"}>
+            <BiLinkExternal /> Explore Movies
+          </Link>
+        </li>
+        <li>
+          <Link target="_blank" to={"https://rcl-contacts-list.netlify.app/"}>
+            <RiContactsBook2Fill /> Manage Contacts
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default LeftBar;
